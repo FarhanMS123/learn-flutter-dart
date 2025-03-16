8fd51232a2d50aab09266ec03bbc518b973c929a
Farhan Sabran <animationfar@gmail.com>
2025-03-16 17:54:02 +0700

    init: very good flutter app

---
+++ tests/vg_flutter_app/android/app/src/main/kotlin/com/example/verygoodcore/vg/flutter/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.vg_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tests/vg_flutter_app/lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:vg_flutter_app/app/app.dart';
+import 'package:vg_flutter_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
